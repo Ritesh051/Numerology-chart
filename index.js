442c15223a1f4d3ee21ef9f74d279a1d7e9dfaa4
Ritesh051 <j4260381@gmail.com>
2024-08-17 23:06:35 +0530

    color change of antardasha and mahadasha

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ themeToggler.addEventListener('click', () => {
         const spans = themeToggler.querySelectorAll('span');
         spans[0].classList.toggle('active');
         spans[1].classList.toggle('active');
-    }, 100);
+    }, 80);
 });
-const populateGridNumbers = (num)=>{
+const populateGridNumbers = (num) => {
     num.split('').forEach(element => {
-        if(element>'0' && element<='9'){
-            gridNumbers.push(element-'0');
+        if (element > '0' && element <= '9') {
+            gridNumbers.push(element - '0');
         }
     });
 }
-const resetData =()=>{
+const resetData = () => {
     days = [];
     planetName = [];
     dayNumberMap = {};
@@ -30,7 +30,7 @@ const resetData =()=>{
 }
 const populateData = () => {
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    planetName = ["Sun","Moon","Jupiter","Rahu","Mercury","Venus","Ketu","Saturn","Mars"];
+    planetName = ["Sun", "Moon", "Jupiter", "Rahu", "Mercury", "Venus", "Ketu", "Saturn", "Mars"];
     dayNumberMap = { "Sunday": 1, "Munday": 2, "Thursday": 3, "Wednesday": 5, "Friday": 6, "Saturday": 8, "Tuesday": 9 };
     gridNumbers = [];
 }
@@ -39,7 +39,7 @@ function getDay(event) {
     resetData();
     populateData();
     const inputDate = document.getElementById('date').value;
-    
+
     const date = new Date(inputDate);
 
     const dayOfWeek = days[date.getDay()];
@@ -50,88 +50,100 @@ function getDay(event) {
     const year = date.getFullYear();
     populateGridNumbers(`${day}`);
     populateGridNumbers(`${month}`);
-    populateGridNumbers(`${year}`.toString().substring(2,4));
+    populateGridNumbers(`${year}`.toString().substring(2, 4));
 
     const dateString = `${day}${month}${year}`;
 
     let destinyNumber = getDestinyNumber(dateString);
     let basicNumber = getBasicNumber(day);
-    const mahaDashaNum = getMahaDasha(basicNumber,year);
-    const antarDashaNum = getAntarDasha(day,month);
-    gridNumbers = [...gridNumbers,basicNumber,destinyNumber,mahaDashaNum,antarDashaNum];
+    const mahaDashaNum = getMahaDasha(basicNumber, year);
+    const antarDashaNum = getAntarDasha(day, month);
+    gridNumbers = [...gridNumbers, basicNumber, destinyNumber, mahaDashaNum, antarDashaNum];
     gridNumbers.sort();
     document.getElementById('Destiny').textContent = destinyNumber;
     document.getElementById('base').textContent = basicNumber;
-    document.getElementById('mahadasha').textContent = mahaDashaNum + " , " + planetName[mahaDashaNum-1];
-    document.getElementById('antardasha').textContent = antarDashaNum + " , " +  planetName[antarDashaNum-1];
+    document.getElementById('mahadasha').textContent = mahaDashaNum + " , " + planetName[mahaDashaNum - 1];
+    document.getElementById('antardasha').textContent = antarDashaNum + " , " + planetName[antarDashaNum - 1];
     document.getElementById('array').textContent = gridNumbers;
     console.log(gridNumbers);
     setGridNumbers();
 }
-const resetGrid = () =>{
+const resetGrid = () => {
     var grid = document.getElementById("grid").children;
-    for(let i = 0; i<grid.length; i++){
+    for (let i = 0; i < grid.length; i++) {
         grid[i].classList.remove('grid-part-selected');
         grid[i].textContent = grid[i].id;
+        grid[i].style.color = "";
     }
 }
 const setGridNumbers = () => {
     let counts = {};
     gridNumbers.forEach(element => {
-        if(counts[element]){
+        if (counts[element]) {
             counts[element]++;
-        }else{
+        } else {
             counts[element] = 1;
         }
-    })
+    });
+
+    const mahaDashaNum = parseInt(document.getElementById('mahadasha').textContent);
+    const antarDashaNum = parseInt(document.getElementById('antardasha').textContent);
+
     var grid = document.getElementById("grid").children;
-    for(let i = 0; i<grid.length; i++){
+    for (let i = 0; i < grid.length; i++) {
         let text = grid[i].textContent;
-        if(counts[text]){
+        if (counts[text]) {
             let content = '';
-            for(let i = 0; i<counts[text]; i++){
-                content+=text
+            for (let j = 0; j < counts[text]; j++) {
+                content += text;
             }
             grid[i].textContent = content;
             grid[i].classList.add('grid-part-selected');
+            if (text == mahaDashaNum) {
+                grid[i].style.color = "red";
+            } else if (text == antarDashaNum) {
+                grid[i].style.color = "green";
+            } else {
+                grid[i].style.color = "white";
+            }
         }
     }
 }
-const getAntarDasha = (day,month)=>{
+
+const getAntarDasha = (day, month) => {
     const currYear = (new Date()).getFullYear();
-    const year = currYear.toString().substring(2,4);
-    const date = new Date(currYear,month-1,day);
+    const year = currYear.toString().substring(2, 4);
+    const date = new Date(currYear, month - 1, day);
     const dayNum = dayNumberMap[days[date.getDay()]];
-    let destinyNumber = getDestinyNumber(`${date.getDate()}${date.getMonth()+1}${year}`);
+    let destinyNumber = getDestinyNumber(`${date.getDate()}${date.getMonth() + 1}${year}`);
     let antarDashaNum = getDestinyNumber(`${destinyNumber}${dayNum}${0}`);
     return antarDashaNum;
-    
 }
-const getBasicNumber = (day)=>{
+const getBasicNumber = (day) => {
     let basicNumber = day;
     while (basicNumber >= 10) {
         basicNumber = basicNumber.toString().split('').map(Number).reduce((acc, num) => acc + num, 0);
     }
     return basicNumber;
 }
-const getDestinyNumber = (dob)=>{
+const getDestinyNumber = (dob) => {
     let destinyNumber = dob.split('').map(Number).reduce((acc, num) => acc + num, 0);
     while (destinyNumber >= 10) {
         destinyNumber = destinyNumber.toString().split('').map(Number).reduce((acc, num) => acc + num, 0);
     }
     return destinyNumber;
 }
-const getMahaDasha = (basicNum,startYear)=>{
+const getMahaDasha = (basicNum, startYear) => {
     const currYear = (new Date()).getFullYear();
     let mahaDasha = basicNum;
-    for(let i = startYear; i<currYear; ){
-        i+=mahaDasha;
-        if(i<currYear){
+    for (let i = startYear; i < currYear;) {
+        i += mahaDasha;
+        if (i < currYear) {
             mahaDasha++;
-            if(mahaDasha>9){
-                mahaDasha=1;
-            }   
-        }     
+            if (mahaDasha > 9) {
+                mahaDasha = 1;
+            }
+        }
     }
     return mahaDasha;
 }
